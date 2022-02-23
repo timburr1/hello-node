8b16491b77d988e0572132df8dbd08a35e82a829
Tim <tim.r.burr@gmail.com>
2022-02-23 11:37:10 -0700

    a few small changes, to make it easier to get started

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname + '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Yay, it works!')
+  res.sendFile(__dirname + '\\public\\index.html')
+  
 })
 
 app.listen(port, () => {
